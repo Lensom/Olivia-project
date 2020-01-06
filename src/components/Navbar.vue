976ad4baa7b41d7e1924a1b5035d787a362e0e0a
Lensom <magurawork@gmail.com>
2020-01-06 23:54:57 +0200

    Change mounted

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -54,7 +54,7 @@ export default {
     }
   },
   mounted() {
-    console.log("this M" + M);
+    // console.log("this M" + M);
     // M.Dropdown.init(this.$refs.dropdown, {
     //   constrainWidth: true
     // });
