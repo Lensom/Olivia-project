97b4ed5afd977f2a4613de5496652269e2e28335
Lensom <magurawork@gmail.com>
2020-01-25 22:09:23 +0200

    Add vuechart js

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,11 +4,25 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
+        <input
+          id="description"
+          type="text"
+          v-model="name"
+          :class="{invalid: $v.name.$dirty && !$v.name.required}"
+        />
         <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <small class="helper-text invalid" v-if="$v.name.$dirty && !$v.name.required">Введите имя</small>
+      </div>
+
+      <div class="switch">
+        <label>
+          English
+          <input type="checkbox" v-model="isRuLocale" />
+          <span class="lever"></span>
+          Русский
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -18,3 +32,49 @@
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import { required } from "vuelidate/lib/validators";
+export default {
+  data: () => ({
+    name: "",
+    isRuLocale: true
+  }),
+  validations: {
+    name: { required }
+  },
+  mounted() {
+    this.name = this.info.name;
+    this.isRuLocale = this.info.locale === "ru-RU";
+    setTimeout(() => {
+      M.updateTextFields();
+    });
+  },
+  computed: {
+    ...mapGetters(["info"])
+  },
+  methods: {
+    ...mapActions(["updateInfo"]),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? "ru-RU" : "en-ES"
+        });
+      } catch (e) {}
+    }
+  }
+};
+</script>
+
+<style scoped>
+.switch {
+  margin-bottom: 2rem;
+}
+</style>
